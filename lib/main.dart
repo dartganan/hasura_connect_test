2b4eb35c659364e796d1a87c0542e0687dc38356
Dartagnan Sabino <dartganan@gmail.com>
2019-08-07 20:49:09 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:hasura_connect/src/app_module.dart';
+
+void main() => runApp(AppModule());
---
+++ lib/src/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:hasura_connect/src/home/home_module.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Slidy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeModule(),
+    );
+  }
+}
